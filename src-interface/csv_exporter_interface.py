8c6a459638a2bc7d4a896766b8101234daf9e71b
Luca Barbaro <luca.barbaro@uniroma1.it>
2025-03-25 10:49:55 +0100

    clean up

--- src-interface/csv_exporter_interface.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-def write_to_csv(constraints, path):
-    with open(path, 'w', newline='') as file:
-        writer = csv.writer(file)
-        for key, value in constraints.items():
-            writer.writerow([key, value])
-    print(f"csv successfully exported to {path}")
--- src-interface/json_exporter_interface.py
+++
@@ -1,6 +0,0 @@
-import json
-
-def write_to_json(output, path):
-    with open(path, 'w') as file:
-        json.dump(output, file)
-    print(f"JSON file successfully exported to {path}")
